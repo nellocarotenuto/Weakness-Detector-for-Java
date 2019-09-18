45ee8c8e2f732f6fca26dbc9abe3b9347136aed3
Nello Carotenuto <nello.carotenuto.96@live.com>
2019-09-18 10:51:50 +0200

    Processing: map more literals to their relative type-bound generic ids

--- src/processing/tokenizer.py
+++ src/processing/tokenizer.py
@@ -1,11 +1,34 @@
 import javalang
 
 
+# Define classes to be mapped to a generic, type-bound identifier
+STR_TYPES = [
+             javalang.tokenizer.Character,
+             javalang.tokenizer.String
+            ]
+INT_TYPES = [
+             javalang.tokenizer.BinaryInteger,
+             javalang.tokenizer.DecimalInteger,
+             javalang.tokenizer.HexInteger,
+             javalang.tokenizer.Integer,
+             javalang.tokenizer.OctalInteger,
+            ]
+FLT_TYPES = [
+             javalang.tokenizer.DecimalFloatingPoint,
+             javalang.tokenizer.FloatingPoint,
+             javalang.tokenizer.HexFloatingPoint
+            ]
+
+# Define generic, type-bound identifiers
 STR_LITERAL = "STR_LITERAL"
 INT_LITERAL = "INT_LITERAL"
 FLT_LITERAL = "FLT_LITERAL"
 
-excluded_token_types = [javalang.tokenizer.Operator, javalang.tokenizer.Separator]
+# Define classes to be ignored when parsing
+excluded_token_types = [
+                        javalang.tokenizer.Operator,
+                        javalang.tokenizer.Separator
+                       ]
 
 
 def tokenize(file):
@@ -28,11 +51,11 @@ def tokenize(file):
 
     for token in parsed_tokens:
         if type(token) not in excluded_token_types:
-            if type(token) is javalang.tokenizer.String:
+            if type(token) in STR_TYPES:
                 tokens.append(STR_LITERAL)
-            elif type(token) is javalang.tokenizer.DecimalInteger:
+            elif type(token) in INT_TYPES:
                 tokens.append(INT_LITERAL)
-            elif type(token) is javalang.tokenizer.DecimalFloatingPoint:
+            elif type(token) in FLT_TYPES:
                 tokens.append(FLT_LITERAL)
             else:
                 tokens.append(token.value)
