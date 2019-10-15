304669249e8ddc5c10dd190c3560455578af7ecd
Nello Carotenuto <nello.carotenuto.96@live.com>
2019-10-15 20:31:17 +0200

    Model builder: create models directory, change RF params and mute warnings

--- src/modeling/model_builder.py
+++ src/modeling/model_builder.py
@@ -1,3 +1,6 @@
+import os
+import warnings
+
 from src.modeling.dataset_splitter import __train_data as train_data
 from src.modeling.dataset_splitter import __train_labels as train_labels
 
@@ -21,6 +24,11 @@ RANDOM_STATE = 7
 
 
 def build_log_reg_models():
+    """
+        Builds various models grid searching a number of scalers, feature selectors and logistic regression parameters.
+    """
+    if not os.path.exists(MODELS_DIR):
+        os.mkdir(MODELS_DIR)
 
     scalers = [None, MinMaxScaler(), StandardScaler()]
     selectors = [None, PCA(), RandomForestRegressor()]
@@ -69,7 +77,7 @@ def build_log_reg_models():
                         param_grid["selector__estimator__n_estimators"] = [10, 100, 1000]
                         param_grid["selector__estimator__random_state"] = [RANDOM_STATE]
                         param_grid["selector__estimator__n_jobs"] = [N_JOBS]
-                        param_grid["selector__threshold"] = [0.05, 0.10]
+                        param_grid["selector__threshold"] = [0.01, 0.025, 0.05]
 
                     steps.append(("selector", SelectFromModel(selector)))
                     classifier_name += "rf-"
@@ -81,8 +89,11 @@ def build_log_reg_models():
 
             pipeline = Pipeline(steps=steps)
 
-            classifier = GridSearchCV(pipeline, param_grid=log_reg_param_grid, cv=10)
-            classifier.fit(train_data, train_labels)
+            with warnings.catch_warnings():
+                warnings.simplefilter(action="ignore", category=UserWarning)
+
+                classifier = GridSearchCV(pipeline, param_grid=log_reg_param_grid, cv=10)
+                classifier.fit(train_data, train_labels)
 
             dump(classifier, f"{MODELS_DIR}/{classifier_name}.joblib")
 
