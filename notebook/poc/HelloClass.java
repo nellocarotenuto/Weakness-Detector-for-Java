80796424b614d99eead51e98850d6f6fd4f8b477
Nello Carotenuto <nello.carotenuto.96@live.com>
2019-10-21 15:38:40 +0200

    Notebook: add a proof of concept

---
+++ notebook/poc/HelloClass.java
@@ -0,0 +1,20 @@
+package greetings;
+
+import java.util.*;
+
+/*
+ * A very simple "Hello World" example in Java
+ */
+public class HelloClass<T extends List> extends Class1.SubType<Type, Int3> implements Interface1, Interface2 {
+
+	public static void main(String[] args) {
+		// Print to standard output
+		int j = 1;
+		j++;
+
+		ArrayList<String> names = new ArrayList<String>();
+
+		System.out.println("Hello World!");
+	}
+
+}
